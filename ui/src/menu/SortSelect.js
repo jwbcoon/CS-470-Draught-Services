60b8d85de7d6d574482512870ea6c9dc407f76d3
jwbcoon <jwbcoon@gmail.com>
2023-10-08 23:09:00 -0700

    added long menu to SortSelect.js

--- ui/src/menu/SortSelect.js
+++ ui/src/menu/SortSelect.js
@@ -1,8 +1,65 @@
-import {useEffect, Fragment} from 'react';
+import {useState, useEffect, Fragment} from 'react';
 import {Grid, Box} from '@mui/material';
 import API from '../API_Interface/API_Interface.js';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-const SortSelect = ({viewColumns, setViewColumns, selectedItem}) => {
+const ITEM_HEIGHT = 48;
+
+const LongMenu = ({selectedItem, options}) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}
+      >
+        {options.map((option) => {
+            const optKey = `${selectedItem.toLowerCase().replace(/s$/, 'ID')}`;
+            return (
+              <MenuItem key={option[optKey]} selected={option[optKey] === options[0][optKey]} onClick={handleClose}>
+                {option[optKey]}
+              </MenuItem>
+            );
+          })
+        }
+      </Menu>
+    </div>
+  );
+};
+
+export default function SortSelect({viewColumns, setViewColumns, selectedItem}) {
     console.log(`In SortSelect: selectedItem is ${selectedItem.toLowerCase()}`);
 
     useEffect(() => {
@@ -18,26 +75,10 @@ const SortSelect = ({viewColumns, setViewColumns, selectedItem}) => {
         if (!selectedItem.match(  /[sS]ummary/)) getViewSelection();
     }, [selectedItem, setViewColumns]);
 
-    console.log(JSON.stringify(viewColumns));
+    console.log(`In SortSelect: viewColumns is ${JSON.stringify(viewColumns)}`);
     if (!selectedItem.match(/[sS]ummary/) || viewColumns === undefined) return <Fragment/>
     return (
-        <Grid container>
-            {
-               viewColumns.map((colName, idx) => {
-                  return (
-                      <Grid item
-                            key={idx}
-                            xs={1}
-                        >
-                          <Box sx={{ height: '100%', width: '100%', backgroundColor: 'white', borderWidth: '1px', borderStyle: 'solid', borderColor: 'black' }}>
-                              {colName}
-                          </Box>
-                      </Grid>
-                  )
-               })
-            }
-        </Grid>
+        <LongMenu selectedItem={selectedItem} options={viewColumns}/>
     );
 }
 
-export default SortSelect;
